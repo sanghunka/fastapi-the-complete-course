d31b9a31711a6b1f0d7f95350f824db915e9844a
sanghun Kang <sanghunka@gmail.com>
2022-06-04 01:31:54 +0900

    103. FastAPI Project: Authentication of a User

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 import models
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
+from fastapi.security import OAuth2PasswordRequestForm
 
 
 class CreateUser(BaseModel):
@@ -37,6 +38,22 @@ def get_password_hash(password):
     return bcrypt_context.hash(password)
 
 
+def verify_password(plain_password, hashed_password):
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(models.Users)\
+        .filter(models.Users.username == username)\
+        .first()
+
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
 @app.post("/create/user")
 async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
@@ -52,3 +69,12 @@ async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)
 
     db.add(create_user_model)
     db.commit()
+
+
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
+                                 db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return "User Validated"
--- TodoApp/models.py
+++ TodoApp/models.py
@@ -21,7 +21,7 @@ class Users(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True)
-    user_name = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
     first_name = Column(String)
     last_name = Column(String)
     hashed_password = Column(String)
