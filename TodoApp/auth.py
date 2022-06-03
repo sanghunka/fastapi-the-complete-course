64a40183156c6e6e04befef54bc7d762ee8085cb
sanghun Kang <sanghunka@gmail.com>
2022-06-04 01:47:44 +0900

    104. FastAPI Project: JSON Web Token (JWT) Creation

--- TodoApp/auth.py
+++ TodoApp/auth.py
@@ -5,7 +5,13 @@ import models
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
-from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from datetime import datetime, timedelta
+from jose import jwt
+
+
+SECRET_KEY = "KlgH6AzYDeZeGwD288to79I3vTHT8wp7"
+ALGORITHM = "HS256"
 
 
 class CreateUser(BaseModel):
@@ -21,6 +27,8 @@ bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 # This will create our databases
 # and do all the necessity things we need for a table
 models.Base.metadata.create_all(bind=engine)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 
 app = FastAPI()
@@ -54,6 +62,17 @@ def authenticate_user(username: str, password: str, db):
     return user
 
 
+def create_access_token(username: str, user_id: int,
+                        expires_delta: Optional[timedelta] = None):
+    encode = {"sub": username, "id": user_id}
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    encode.update({"exp": expire})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
 @app.post("/create/user")
 async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
@@ -77,4 +96,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
     user = authenticate_user(form_data.username, form_data.password, db)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    return "User Validated"
+    token_expires = timedelta(minutes=20)
+    token = create_access_token(user.username,
+                                user.id,
+                                expires_delta=token_expires)
+    return {"token": token}
