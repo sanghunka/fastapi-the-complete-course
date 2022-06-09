f027c7316e2f98964d6cfdc0e72efb368ad30ccf
sanghun Kang <sanghunka@gmail.com>
2022-06-09 11:09:10 +0900

    135. FastAPI Project - External Routing

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 import models
 from database import engine
 from routers import auth, todos
+from company import companyapis
 
 
 app = FastAPI()
@@ -10,3 +11,9 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
+app.include_router(
+    companyapis.router,
+    prefix="/companyapis",
+    tags=["companyapis"],
+    responses={418: {"description": "Internal Use Only"}}
+)
