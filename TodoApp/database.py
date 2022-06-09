4d9c4bce46a3e35f11d614b1bbcecc662f39da1d
sanghun Kang <sanghunka@gmail.com>
2022-06-09 10:34:44 +0900

    Rollback to use sqlite

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -3,17 +3,17 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
-
-# engine = create_engine(
-#     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-# )
-
-SQLALCHEMY_DATABASE_URL = "postgresql://sanghunka:test1234!@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
+
+# SQLALCHEMY_DATABASE_URL = "postgresql://sanghunka:test1234!@localhost/TodoApplicationDatabase"
+#
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL
+# )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
