be89eeee68da34f63148f2faed283076b53c3e54
sanghun Kang <sanghunka@gmail.com>
2022-06-09 10:43:05 +0900

    132. FastAPI Project - Authentication Routing

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI, Depends, HTTPException, status
+import sys
+sys.path.append("..")
+
+
+from fastapi import Depends, HTTPException, status, APIRouter
 from pydantic import BaseModel
 from typing import Optional
 import models
@@ -31,8 +35,8 @@ models.Base.metadata.create_all(bind=engine)
 oauth2_bearer = OAuth2PasswordBearer(tokenUrl="token")
 
 
-app = FastAPI()
-
+# app = FastAPI()
+router = APIRouter()
 
 def get_db():
     try:
@@ -85,7 +89,7 @@ async def get_current_user(token: str = Depends(oauth2_bearer)):
         raise get_user_exception()
 
 
-@app.post("/create/user")
+@router.post("/create/user")
 async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
     create_user_model = models.Users()
     create_user_model.email = create_user.email
@@ -102,7 +106,7 @@ async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)
     db.commit()
 
 
-@app.post("/token")
+@router.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(),
                                  db: Session = Depends(get_db)):
     user = authenticate_user(form_data.username, form_data.password, db)
