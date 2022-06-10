1e20b38ac1d1ca5b167fed2e84e19eec604acadb
sanghun Kang <sanghunka@gmail.com>
2022-06-10 09:59:57 +0900

    146. Alembic Enhance Application With New API

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Depends
 import models
 from database import engine
-from routers import auth, todos,users
+from routers import auth, todos, users, address
 from company import companyapis, dependencies
 
 
@@ -12,6 +12,7 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(users.router)
+app.include_router(address.router)
 app.include_router(
     companyapis.router,
     prefix="/companyapis",
