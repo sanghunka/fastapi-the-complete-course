40b2550a4b9da5db1d69d9e7ca2cccc5ccaed6a6
sanghunka <sanghunka@gmail.com>
2022-06-11 22:47:10 +0900

    167. FastAPI Full Stack - Add Todo

--- routers/todos.py
+++ routers/todos.py
@@ -1,8 +1,11 @@
 import sys
 sys.path.append("../TodoApp")
 
+from starlette import status
+from starlette.responses import RedirectResponse
+
 from typing import Optional
-from fastapi import Depends, HTTPException, APIRouter, Request
+from fastapi import Depends, HTTPException, APIRouter, Request, Form
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -53,6 +56,22 @@ async def read_all_by_user(request: Request, db: Session = Depends(get_db)):
 @router.get("/add-todo", response_class=HTMLResponse)
 async def add_new_todo(request: Request):
     return templates.TemplateResponse("add-todo.html", {"request": request})
+
+
+@router.post("/add-todo", response_class=HTMLResponse)
+async def create_todo(request: Request, title: str = Form(...), description: str = Form(...),
+                      priority: int = Form(...), db: Session = Depends(get_db)):
+    todo_model = models.Todos()
+    todo_model.title = title
+    todo_model.description = description
+    todo_model.priority = priority
+    todo_model.complete = False
+    todo_model.owner_id = 1
+
+    db.add(todo_model)
+    db.commit()
+
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
 
 
 @router.get("/edit-todo/{todo_id}", response_class=HTMLResponse)
