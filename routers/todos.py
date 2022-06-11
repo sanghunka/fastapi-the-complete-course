53f3044cf46d5172aa5ea72de4dc144c0723c820
sanghunka <sanghunka@gmail.com>
2022-06-11 21:40:16 +0900

    158. FastAPI Full Stack - Full Guide: HTML Part 2

--- routers/todos.py
+++ routers/todos.py
@@ -44,7 +44,7 @@ class Todo(BaseModel):
 
 @router.get("/test")
 async def test(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    return templates.TemplateResponse("add-todo.html", {"request": request})
 
 
 @router.get("/")
