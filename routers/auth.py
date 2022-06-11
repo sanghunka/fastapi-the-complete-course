dc572e868f1d4b604b33ddfe50ea50fdc0ef09dc
sanghunka <sanghunka@gmail.com>
2022-06-11 22:11:24 +0900

    163. FastAPI Full Stack - APIs for HTML

--- routers/auth.py
+++ routers/auth.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("../TodoApp")
 
 
-from fastapi import Depends, HTTPException, status, APIRouter
+from fastapi import Depends, HTTPException, status, APIRouter, Request
 from pydantic import BaseModel
 from typing import Optional
 import models
@@ -13,9 +13,14 @@ from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from datetime import datetime, timedelta
 from jose import jwt, JWTError
 
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 SECRET_KEY = "KlgH6AzYDeZeGwD288to79I3vTHT8wp7"
 ALGORITHM = "HS256"
+
+
+templates = Jinja2Templates(directory="templates")
 
 
 class CreateUser(BaseModel):
@@ -124,6 +129,16 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
                                 user.id,
                                 expires_delta=token_expires)
     return {"token": token}
+
+
+@router.get("/", response_class=HTMLResponse)
+async def authentication_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+@router.get("/register", response_class=HTMLResponse)
+async def register(request: Request):
+    return templates.TemplateResponse("register.html", {"request": request})
 
 
 #Exceptions
