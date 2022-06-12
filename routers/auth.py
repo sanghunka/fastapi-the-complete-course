c0e6188890827c260d781f51c34d8d06ccc12876
sanghunka <sanghunka@gmail.com>
2022-06-12 11:22:31 +0900

    176. FastAPI Full Stack - Code Clean Up

--- routers/auth.py
+++ routers/auth.py
@@ -25,13 +25,13 @@ ALGORITHM = "HS256"
 templates = Jinja2Templates(directory="templates")
 
 
-class CreateUser(BaseModel):
-    username: str
-    email: Optional[str]
-    first_name: str
-    last_name: str
-    password: str
-    phone_number: Optional[str]
+# class CreateUser(BaseModel):
+#     username: str
+#     email: Optional[str]
+#     first_name: str
+#     last_name: str
+#     password: str
+#     phone_number: Optional[str]
 
 
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -123,28 +123,28 @@ async def get_current_user(request: Request):
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
         if username is None or user_id is None:
-            return None
+            logout(request)
         return {"username": username, "id": user_id}
     except JWTError:
         raise get_user_exception()
 
 
-@router.post("/create/user")
-async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
-    create_user_model = models.Users()
-    create_user_model.email = create_user.email
-    create_user_model.username = create_user.username
-    create_user_model.first_name = create_user.first_name
-    create_user_model.last_name = create_user.last_name
-    create_user_model.phone_number = create_user.phone_number
-
-    hash_password = get_password_hash(create_user.password)
-
-    create_user_model.hashed_password = hash_password
-    create_user_model.is_active = True
-
-    db.add(create_user_model)
-    db.commit()
+# @router.post("/create/user")
+# async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
+#     create_user_model = models.Users()
+#     create_user_model.email = create_user.email
+#     create_user_model.username = create_user.username
+#     create_user_model.first_name = create_user.first_name
+#     create_user_model.last_name = create_user.last_name
+#     create_user_model.phone_number = create_user.phone_number
+#
+#     hash_password = get_password_hash(create_user.password)
+#
+#     create_user_model.hashed_password = hash_password
+#     create_user_model.is_active = True
+#
+#     db.add(create_user_model)
+#     db.commit()
 
 
 @router.post("/token")
@@ -230,19 +230,19 @@ async def register_user(request: Request, email: str = Form(...), username: str 
 
 
 #Exceptions
-def get_user_exception():
-    credentials_exception = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"}
-    )
-    return credentials_exception
-
-
-def token_exception():
-    token_exception_response = HTTPException(
-        status_code=status.HTTP_401_UNAUTHORIZED,
-        detail="Incorrect username or password",
-        headers={"WWW-Authenticate": "Bearer"},
-    )
-    return token_exception_response
+# def get_user_exception():
+#     credentials_exception = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Could not validate credentials",
+#         headers={"WWW-Authenticate": "Bearer"}
+#     )
+#     return credentials_exception
+#
+#
+# def token_exception():
+#     token_exception_response = HTTPException(
+#         status_code=status.HTTP_401_UNAUTHORIZED,
+#         detail="Incorrect username or password",
+#         headers={"WWW-Authenticate": "Bearer"},
+#     )
+#     return token_exception_response
