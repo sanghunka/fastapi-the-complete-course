31a9bc50aacae7903eb44d666a0bee41b7ff1864
sanghunka <sanghunka@gmail.com>
2022-06-12 16:34:36 +0900

    193. Heroku PostgreSQL: Production Database

--- database.py
+++ database.py
@@ -1,3 +1,5 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,7 +11,10 @@ from sqlalchemy.ext.declarative import declarative_base
 #     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 # )
 
-SQLALCHEMY_DATABASE_URL = "postgresql://sanghunka:test1234!@localhost/TodoApplicationDatabase"
+# SQLALCHEMY_DATABASE_URL = "postgresql://sanghunka:test1234!@localhost/TodoApplicationDatabase"
+SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
+if SQLALCHEMY_DATABASE_URL.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URL = SQLALCHEMY_DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
