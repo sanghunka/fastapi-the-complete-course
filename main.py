4e64260c382b5730a93f3a81f7b8e7fb30853627
sanghunka <sanghunka@gmail.com>
2022-06-12 11:35:33 +0900

    177. FastAPI Full Stack - Code Clean Up Part 2

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from database import engine
 from routers import auth, todos, users, address
 from company import companyapis, dependencies
 from starlette.staticfiles import StaticFiles
+from starlette import status
+from starlette.responses import RedirectResponse
 
 
 app = FastAPI()
@@ -11,6 +13,11 @@ app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/")
+async def root():
+    return RedirectResponse(url="/todos", status_code=status.HTTP_302_FOUND)
 
 app.include_router(auth.router)
 app.include_router(todos.router)
--- routers/auth.py
+++ routers/auth.py
@@ -126,7 +126,7 @@ async def get_current_user(request: Request):
             logout(request)
         return {"username": username, "id": user_id}
     except JWTError:
-        raise get_user_exception()
+        raise HTTPException(status_code=404, detail="Not Found")
 
 
 # @router.post("/create/user")
@@ -230,19 +230,19 @@ async def register_user(request: Request, email: str = Form(...), username: str 
 
 
 #Exceptions
-# def get_user_exception():
-#     credentials_exception = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Could not validate credentials",
-#         headers={"WWW-Authenticate": "Bearer"}
-#     )
-#     return credentials_exception
-#
-#
-# def token_exception():
-#     token_exception_response = HTTPException(
-#         status_code=status.HTTP_401_UNAUTHORIZED,
-#         detail="Incorrect username or password",
-#         headers={"WWW-Authenticate": "Bearer"},
-#     )
-#     return token_exception_response
+def get_user_exception():
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"}
+    )
+    return credentials_exception
+
+
+def token_exception():
+    token_exception_response = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Incorrect username or password",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    return token_exception_response
